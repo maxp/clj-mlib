333364710f1a110808d27e37d8eea3d1b39ddaeb
Maxim Penzin <maxp@ya.ru>
2020-04-22 21:30:33 +0800

    http: parse-request-authorization

--- src/mlib/http/util.clj
+++ src/mlib/http/util.clj
@@ -1,11 +1,12 @@
 (ns mlib.http.util
   (:require
-    [clojure.string   :refer  [split]]
+    [clojure.string   :refer  [split blank? lower-case]]
     [clojure.java.io  :refer  [input-stream]]
     [jsonista.core    :refer  
       [object-mapper read-value write-value-as-bytes write-value-as-string]])) 
 ;=
 
+;; NOTE: deprecated
 (defn authorization-split [req]
   (when-let [auth (get-in req [:headers "authorization"])]
     (when (string? auth)
@@ -19,7 +20,47 @@
 
   ,)
 
-;; 
+;; ;; ;; ;; ;; ;; ;; ;; ;; ;;
+
+(defn- split-pair [s]
+  (let [[k v] (split s #"\s+" 2)]
+    (when (and k (not (blank? k)))
+      [ (keyword (lower-case k))
+        (or v "")])))
+;-
+      
+(defn parse-request-authorization [req]
+  (when-let [auth (get-in req [:headers "authorization"])]
+    (cond
+      (string? auth)  
+      (when-let [pair (split-pair auth)]
+        [pair])
+      ;
+      (vector? auth)  
+      (reduce
+        (fn [acc s]
+          (if-let [pair (split-pair s)]
+            (conj acc pair)
+            acc))
+        []
+        auth)
+      ;
+      :else nil)))
+;=
+
+(comment
+
+  (into {}
+    (parse-request-authorization
+      {:headers {"authorization" ["Bearer qwe123" "basic  123" "Session sid.key" "AWS"]}}))
+
+  (into {}
+    (parse-request-authorization
+      {:headers {"authorization" "Bearer qwe123"}}))
+
+  ,)
+
+;; ;; ;; ;; ;; ;; ;; ;; ;; ;;
 
 (defn x-real-ip [req]
   (let [headers (get req :headers)]
