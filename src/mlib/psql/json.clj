75cf885441f772a149025fa99994b989d718d0fd
Maxim Penzin <maxp@ya.ru>
2020-05-08 14:47:21 +0800

    psql: sql primitive arrays support

--- src/mlib/psql/json.clj
+++ src/mlib/psql/json.clj
@@ -1,13 +1,20 @@
 (ns mlib.psql.json
   (:import
+    [java.sql             Array PreparedStatement]
     [org.postgresql.util  PGobject])
   (:require 
-    [jsonista.core        :as json]
-    [next.jdbc.prepare    :as prepare]
-    [next.jdbc.result-set :as rs]))
+    [jsonista.core        :as    json]
+    [next.jdbc.date-time  :refer [read-as-instant]]
+    [next.jdbc.prepare    :refer [SettableParameter]]
+    [next.jdbc.result-set :refer [ReadableColumn]]))
 ;=
 
-;; https://cljdoc.org/d/seancorfield/next.jdbc/1.0.409/doc/getting-started/tips-tricks
+(set! *warn-on-reflection* true)
+
+;; java.time.Instant conversion
+(read-as-instant)
+
+;; https://cljdoc.org/d/seancorfield/next.jdbc/1.0.424/doc/getting-started/tips-tricks
 
 (def mapper (json/object-mapper {:decode-key-fn keyword}))
 (def ->json json/write-value-as-string)
@@ -24,7 +31,7 @@
 
 (defn <-pgobject
   "Transform PGobject containing `json` or `jsonb` value to Clojure data."
-  [^org.postgresql.util.PGobject v]
+  [^PGobject v]
   (let [type  (.getType v)
         value (.getValue v)]
     (if (#{"jsonb" "json"} type)
@@ -35,23 +42,28 @@
 
 ;; if a SQL parameter is a Clojure hash map or vector, it'll be transformed
 ;; to a PGobject for JSON/JSONB:
-(extend-protocol prepare/SettableParameter
+(extend-protocol SettableParameter
   clojure.lang.IPersistentMap
-  (set-parameter [m s i]
+  (set-parameter [m, ^PreparedStatement s, i]
     (.setObject s i (->pgobject m)))
 
   clojure.lang.IPersistentVector
-  (set-parameter [v s i]
+  (set-parameter [v, ^PreparedStatement s, i]
     (.setObject s i (->pgobject v))))
 ;;
 
 ;; if a row contains a PGobject then we'll convert them to Clojure data
 ;; while reading (if column is either "json" or "jsonb" type):
-(extend-protocol rs/ReadableColumn
-  org.postgresql.util.PGobject
-  (read-column-by-label [^org.postgresql.util.PGobject v _]
-    (<-pgobject v))
-  (read-column-by-index [^org.postgresql.util.PGobject v _2 _3]
-    (<-pgobject v)))
+(extend-protocol ReadableColumn
+  PGobject
+  (read-column-by-label [^PGobject v _]     (<-pgobject v))
+  (read-column-by-index [^PGobject v _2 _3] (<-pgobject v)))
 ;;
 
+;; sql Array -> Vector
+;; 
+(extend-protocol ReadableColumn
+  Array
+  (read-column-by-label [^Array v _]    (vec (.getArray v)))
+  (read-column-by-index [^Array v _ _]  (vec (.getArray v))))
+;;
